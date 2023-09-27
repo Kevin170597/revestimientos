b98f9971add6d5054449f4048a23fb6b4db1773c
Kevin Mendoza <kevin@xymilar.com>
2023-09-27 16:12:53 -0300

    css scroll bar

--- app/anti-humedad/page.tsx
+++ app/anti-humedad/page.tsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.css"
 import Card from "@/components/Card/Card"
+import Image from "next/image"
 import { getAntiHumidity } from "./services/anti-humedad.service"
 
 const fetchAntiHumidity = () => {
@@ -8,9 +9,17 @@ const fetchAntiHumidity = () => {
 
 export default function AntiHumidity() {
     const antiHumidity = fetchAntiHumidity()
-    
+
     return (
         <div className={styles.antiHumidity}>
+            <Image
+            className={styles.cover}
+                width={0}
+                height={0}
+                sizes="100vw"
+                src={"/cover-antihumedad2.png"}
+                alt="portada"
+            />
             <div className={styles.cardsList}>
                 {antiHumidity.map((e) =>
                     <Card placa={e} key={e.id} />
